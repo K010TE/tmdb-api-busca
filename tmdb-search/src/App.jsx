02f91ad2db7c8593b745cd7fff54b33d9996bb21
Itagiba <79667685+K010TE@users.noreply.github.com>
2024-12-04 03:58:45 +0000

    adiciona chave da API em um arquivo .env para a segurança

--- tmdb-search/src/App.jsx
+++ tmdb-search/src/App.jsx
@@ -1,47 +1,59 @@
-const token = '' //INSIRA A SUA API KEY ENTRE AS ASPAS DA CONSTANTE TOKEN 
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-import React, { useState } from 'react';
-import axios from 'axios';
-import './App.css';
+const token = import.meta.env.VITE_API_TOKEN; //INSIRA A SUA API KEY ENTRE AS ASPAS DA CONSTANTE TOKEN
+
+console.log("Variáveis carregadas:", import.meta.env);
+console.log("API Token:", import.meta.env.VITE_API_KEY);
 
 const App = () => {
-  const [movieName, setMovieName] = useState('');
+  const [movieName, setMovieName] = useState("");
   const [results, setResults] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
 
   const handleSearch = async () => {
     const apiKey = token;
-    const searchUrl = `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(movieName)}&api_key=${apiKey}`;
+    const searchUrl = `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(
+      movieName
+    )}&api_key=${apiKey}`;
 
     try {
       const searchResponse = await axios.get(searchUrl);
-      const movieDetailsPromises = searchResponse.data.results.map(async (movie) => {
-        const detailsUrl = `https://api.themoviedb.org/3/movie/${movie.id}?api_key=${apiKey}&append_to_response=credits`;
-        const detailsResponse = await axios.get(detailsUrl);
+      const movieDetailsPromises = searchResponse.data.results.map(
+        async (movie) => {
+          const detailsUrl = `https://api.themoviedb.org/3/movie/${movie.id}?api_key=${apiKey}&append_to_response=credits`;
+          const detailsResponse = await axios.get(detailsUrl);
 
-        const movieDetails = detailsResponse.data;
-        const director = movieDetails.credits.crew.find(
-          (member) => member.job === 'Director'
-        );
+          const movieDetails = detailsResponse.data;
+          const director = movieDetails.credits.crew.find(
+            (member) => member.job === "Director"
+          );
 
-        return {
-          title: movieDetails.title,
-          year: movieDetails.release_date ? movieDetails.release_date.split('-')[0] : 'N/A',
-          country: movieDetails.production_countries.length > 0 ? movieDetails.production_countries[0].name : 'N/A',
-          director: director ? director.name : 'N/A',
-          poster: movieDetails.poster_path
-            ? `https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`
-            : 'generic-poster.png'
-        };
-      });
+          return {
+            title: movieDetails.title,
+            year: movieDetails.release_date
+              ? movieDetails.release_date.split("-")[0]
+              : "N/A",
+            country:
+              movieDetails.production_countries.length > 0
+                ? movieDetails.production_countries[0].name
+                : "N/A",
+            director: director ? director.name : "N/A",
+            poster: movieDetails.poster_path
+              ? `https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`
+              : "generic-poster.png",
+          };
+        }
+      );
 
       let movieDetails = await Promise.all(movieDetailsPromises);
 
       // Ordenar os resultados pelo ano de lançamento
       movieDetails = movieDetails.sort((a, b) => {
-        if (a.year === 'N/A') return 1;
-        if (b.year === 'N/A') return -1;
+        if (a.year === "N/A") return 1;
+        if (b.year === "N/A") return -1;
         return b.year - a.year;
       });
 
@@ -49,12 +61,12 @@ const App = () => {
       setCurrentPage(1);
       setTotalPages(Math.ceil(movieDetails.length / 10));
     } catch (error) {
-      console.error('Error fetching movie details:', error);
+      console.error("Error fetching movie details:", error);
     }
   };
 
   const handleKeyPress = (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       handleSearch();
     }
   };
@@ -85,7 +97,11 @@ const App = () => {
             {currentResults.map((movie, index) => (
               <li key={index} className="movie-item">
                 <img
-                  src={movie.poster !== 'N/A' ? movie.poster : '/generic-poster.png'}
+                  src={
+                    movie.poster !== "N/A"
+                      ? movie.poster
+                      : "/generic-poster.png"
+                  }
                   alt={`${movie.title} Poster`}
                   className="movie-poster"
                 />
@@ -102,7 +118,9 @@ const App = () => {
             {Array.from({ length: totalPages }, (_, index) => (
               <button
                 key={index}
-                className={`page-button ${currentPage === index + 1 ? 'active' : ''}`}
+                className={`page-button ${
+                  currentPage === index + 1 ? "active" : ""
+                }`}
                 onClick={() => handlePageChange(index + 1)}
               >
                 {index + 1}
--- tmdb-search/vite.config.js
+++ tmdb-search/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+import path from "path";
 
-// https://vitejs.dev/config/
+// Ajustar o caminho do arquivo .env para a raiz do projeto
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+console.log("Variáveis carregadas pelo dotenv:", process.env.VITE_API_TOKEN);
+
 export default defineConfig({
   plugins: [react()],
-})
+});
